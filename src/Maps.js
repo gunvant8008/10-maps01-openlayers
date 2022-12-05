6f9ab8a5413fac6e98c92c760ba8c1c0b3b84f6e
Gunvant Sharma <i.gunvant.sharma@gmail.com>
2022-12-05 19:51:21 +0000

    Update Maps.js

--- src/Maps.js
+++ src/Maps.js
@@ -1,29 +1,27 @@
-import Map from "ol/Map.js"
-import Group from "ol/layer/Group"
-import View from "ol/View.js"
-import "ol/ol.css"
-import "ol-layerswitcher/dist/ol-layerswitcher.css"
-import LayerSwitcher from "ol-layerswitcher"
-import MousePosition from "ol/control/MousePosition"
-import { format } from "ol/coordinate"
-import ScaleLine from "ol/control/ScaleLine"
-import { useEffect, useRef, useState } from "react"
-import home from "./assets/home.png"
-import fs from "./assets/fullScreen.png"
-import zoomIn from "./assets/zoomIn.png"
-import zoomOut from "./assets/zoomOut.png"
-import DragBox from "ol/interaction/DragBox"
-import Control from "ol/control/Control"
-import { getCenter } from "ol/extent"
+import Map from "ol/Map.js";
+import Group from "ol/layer/Group";
+import View from "ol/View.js";
+import "ol/ol.css";
+import "ol-layerswitcher/dist/ol-layerswitcher.css";
+import LayerSwitcher from "ol-layerswitcher";
+import MousePosition from "ol/control/MousePosition";
+import { format } from "ol/coordinate";
+import ScaleLine from "ol/control/ScaleLine";
+import { useEffect, useRef, useState } from "react";
+import home from "./assets/home.png";
+import fs from "./assets/fullScreen.png";
+import zoomIn from "./assets/zoomIn.png";
+//import zoomOut from "./assets/zoomOut.png"
+import DragBox from "ol/interaction/DragBox";
+import Control from "ol/control/Control";
 
 const Maps = ({ baselayers, overlays }) => {
-  const popupRef = useRef(null)
-  const homeRef = useRef(null)
-  const fsRef = useRef(null)
-  const zoomInRef = useRef(null)
-  const zoomOutRef = useRef(null)
+  const popupRef = useRef(null);
+  const homeRef = useRef(null);
+  const fsRef = useRef(null);
+  const zoomInRef = useRef(null);
   // eslint-disable-next-line
-  const [map, setMap] = useState(null)
+  const [map, setMap] = useState(null);
 
   // useEffect to initialize map
   useEffect(() => {
@@ -35,46 +33,46 @@ const Maps = ({ baselayers, overlays }) => {
         zoom: 2,
         maxZoom: 10,
         minZoom: 3,
-        rotation: 0
+        rotation: 0,
       }),
-      controls: []
-    })
+      controls: [],
+    });
 
-    setMap(mapObject)
+    setMap(mapObject);
 
     return () => {
-      mapObject.setTarget(undefined)
-    }
+      mapObject.setTarget(undefined);
+    };
     // eslint-disable-next-line
-  }, [])
+  }, []);
 
   useEffect(() => {
-    if (!map) return
+    if (!map) return;
 
     // Overlay Group
     const overlayGroup = new Group({
       title: "overlays",
       layers: overlays,
-      fold: true
-    })
+      fold: true,
+    });
 
     // // Layer Group
     const baseLayerGroup = new Group({
       title: "Base maps",
       layers: baselayers,
-      fold: true
-    })
+      fold: true,
+    });
 
-    map.addLayer(baseLayerGroup)
-    map.addLayer(overlayGroup)
+    map.addLayer(baseLayerGroup);
+    map.addLayer(overlayGroup);
 
     const layerSwitcher = new LayerSwitcher({
       activationMode: "click",
       startActive: false,
       reverse: true,
-      groupSelectStyle: "group"
-    })
-    map.addControl(layerSwitcher)
+      groupSelectStyle: "group",
+    });
+    map.addControl(layerSwitcher);
 
     // coordinate mouse Position
     const mousePosition = new MousePosition({
@@ -82,138 +80,105 @@ const Maps = ({ baselayers, overlays }) => {
         " fixed w-[220px] bottom-2 left-[35%] border-2 border-zinc-500 text-center bg-gray-100 rounded-lg hover:pointer-events-none",
       projection: "EPSG:4326",
       coordinateFormat: function (coordinate) {
-        return format(coordinate, `{y}, {x}`, 6)
-      }
-    })
-    map.addControl(mousePosition)
+        return format(coordinate, `{y}, {x}`, 6);
+      },
+    });
+    map.addControl(mousePosition);
 
     // scale line on map
     const scaleControl = new ScaleLine({
       bar: true,
-      text: true
-    })
-    map.addControl(scaleControl)
-  }, [map, baselayers, overlays])
+      text: true,
+    });
+    map.addControl(scaleControl);
+  }, [map, baselayers, overlays]);
 
   //console.log(overlays)
   //console.log(popupRef.current)
 
   // working on feature info section, we can add it as overlay and can set its position so that it will show on the clicked feature
-  const info = popupRef.current
+  const info = popupRef.current;
   if (map) {
-    map.on("singleclick", showInfo)
+    map.on("singleclick", showInfo);
     function showInfo(event) {
-      const features = map.getFeaturesAtPixel(event.pixel)
+      const features = map.getFeaturesAtPixel(event.pixel);
       if (features.length === 0) {
-        info.innerText = ""
-        info.style.opacity = 0
-        return
+        info.innerText = "";
+        info.style.opacity = 0;
+        return;
       }
       //console.log(features[0])
-      const properties = features[0].getProperties()
+      const properties = features[0].getProperties();
       //console.log(properties)
-      const name = properties.naam
-      const no = properties.nr
-      const size = properties.hectares
+      const name = properties.naam;
+      const no = properties.nr;
+      const size = properties.hectares;
 
       //console.log(name, no, size)
 
-      info.innerHTML = `NATIONAL PARK <br> Name: ${name} <br> No: ${no} <br> Size: ${size} h `
-      info.style.opacity = 1
+      info.innerHTML = `NATIONAL PARK <br> Name: ${name} <br> No: ${no} <br> Size: ${size} h `;
+      info.style.opacity = 1;
     }
   }
 
   // adding Home Control
-  const homeElement = homeRef.current
+  const homeElement = homeRef.current;
   const homeControl = new Control({
-    element: homeElement
-  })
+    element: homeElement,
+  });
   if (map) {
-    map.addControl(homeControl)
+    map.addControl(homeControl);
   }
 
   // adding full screen control
-  const fsElement = fsRef.current
+  const fsElement = fsRef.current;
   const fsControl = new Control({
-    element: fsElement
-  })
-  const mapRef = useRef(null)
-  const mapElement = mapRef.current
+    element: fsElement,
+  });
+  const mapRef = useRef(null);
+  const mapElement = mapRef.current;
   const fsFunction = () => {
     if (mapElement.requestFullScreen) {
-      mapElement.requestFullScreen()
+      mapElement.requestFullScreen();
     } else if (mapElement.msRequestFullScreen) {
-      mapElement.msrequestFullScreen()
+      mapElement.msrequestFullScreen();
     } else if (mapElement.mozRequestFullScreen) {
-      mapElement.mozrequestFullScreen()
+      mapElement.mozrequestFullScreen();
     } else if (mapElement.webkitRequestFullScreen) {
-      mapElement.webkitRequestFullScreen()
+      mapElement.webkitRequestFullScreen();
     }
-  }
+  };
   if (map) {
-    map.addControl(fsControl)
+    map.addControl(fsControl);
   }
 
   // adding zoom In control button
-  //const [zoomInFlag, setZoomInFlag] = useState(false)
-  const zoomInElement = zoomInRef.current
+  const [zoomInFlag, setZoomInFlag] = useState(false);
+  const zoomInElement = zoomInRef.current;
+
   const zoomInControl = new Control({
-    element: zoomInElement
-  })
-  const zoomInInteraction = new DragBox()
+    element: zoomInElement,
+  });
+
+  const zoomInInteraction = new DragBox();
   zoomInInteraction.on("boxend", function () {
-    const zoomInExtent = zoomInInteraction.getGeometry().getExtent()
-    map.getView().fit(zoomInExtent)
-  })
-
-  let zoomInFlag = false
-  // const zoomInFlag = useRef(false)
-  const zoomInFn = () => {
-    // setZoomInFlag(!zoomInFlag)
-    zoomInFlag = !zoomInFlag
-    //zoomInFlag.current = !zoomInFlag.current
+    var zoomInExtent = zoomInInteraction.getGeometry().getExtent();
+    map.getView().fit(zoomInExtent);
+  });
+  const zoomInFn = (e) => {
+     e.preventDefault();
+    setZoomInFlag((prev) => !prev);
     if (zoomInFlag) {
-      map.addInteraction(zoomInInteraction)
-      mapElement.style.cursor = "zoom-in"
+      mapElement.style.cursor = "zoom-in";
+      map.addInteraction(zoomInInteraction);
     } else {
-      map.removeInteraction(zoomInInteraction)
-      mapElement.style.cursor = "default"
+      map.removeInteraction(zoomInInteraction);
+      mapElement.style.cursor = "default";
     }
-  }
-
+  };
+  //console.log(zoomInFlag);
   if (map) {
-    map.addControl(zoomInControl)
-  }
-
-  // adding zoom Out control button
-  const zoomOutElement = zoomOutRef.current
-  const zoomOutControl = new Control({
-    element: zoomOutElement
-  })
-  const zoomOutInteraction = new DragBox()
-  zoomOutInteraction.on("boxend", function () {
-    const zoomOutExtent = zoomOutInteraction.getGeometry().getExtent()
-    if (map) {
-      map.getView().setCenter(getCenter(zoomOutExtent))
-      map.getView().setZoom(map.getView().getZoom() - 1)
-      //  map.view.setZoom(map.view.getZoom() - 1)
-    }
-  })
-
-  let zoomOutFlag = false
-  const zoomOutFn = () => {
-    zoomOutFlag = !zoomOutFlag
-    if (zoomOutFlag) {
-      map.addInteraction(zoomOutInteraction)
-      mapElement.style.cursor = "zoom-out"
-    } else {
-      map.removeInteraction(zoomOutInteraction)
-      mapElement.style.cursor = "default"
-    }
-  }
-
-  if (map) {
-    map.addControl(zoomOutControl)
+    map.addControl(zoomInControl);
   }
 
   return (
@@ -229,7 +194,7 @@ const Maps = ({ baselayers, overlays }) => {
       >
         <button
           onClick={() => {
-            window.location.href = ""
+            window.location.href = "";
           }}
         >
           <img className="w-[25px] h-[25px]" src={home} alt="home" />
@@ -253,25 +218,12 @@ const Maps = ({ baselayers, overlays }) => {
             : "absolute top-[115px] left-4 z-20 border-2 border-blue-300 rounded-md bg-blue-100 hover:bg-white p-1 "
         }
       >
-        <button onClick={() => zoomInFn()}>
-          <img className="w-[25px] h-[25px]" src={zoomIn} alt="zoomIn" />
-        </button>
-      </div>
-      {/* Zoom Out button */}
-      <div
-        ref={zoomOutRef}
-        className={
-          zoomOutFlag
-            ? " absolute top-[165px] left-4 z-20 border-2 border-blue-300 rounded-md bg-green-100 hover:bg-white p-1 "
-            : "absolute top-[165px] left-4 z-20 border-2 border-blue-300 rounded-md bg-blue-100 hover:bg-white p-1 "
-        }
-      >
-        <button onClick={() => zoomOutFn()}>
-          <img className="w-[25px] h-[25px]" src={zoomOut} alt="zoomOut" />
+        <button onClick={(e) => zoomInFn(e)}>
+          <img className="w-[25px] h-[25px]" src={zoomIn} alt="home" />
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Maps
+export default Maps;
